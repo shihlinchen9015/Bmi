a8e6b4052e61a8759f58058d9908fc6ed29c353f
shihlinchen9015 <shihlinchen9015@gmail.com>
2020-01-14 12:45:39 +0800

    文字方塊、按鈕、浮動文字互動與設計

--- app/src/main/java/com/shihlinchen9015/bmi/MainActivity.java
+++ app/src/main/java/com/shihlinchen9015/bmi/MainActivity.java
@@ -6,23 +6,37 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edWeight;
+    private EditText edHeight;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+    }
+
+    private void findViews() {
+        edWeight = findViewById(R.id.ed_weight);
+        edHeight = findViewById(R.id.ed_height);
+        result = findViewById(R.id.result);
     }
 
     public void bmi(View view) {
-        EditText edWeight = findViewById(R.id.ed_weight);
-        EditText edHeight = findViewById(R.id.ed_height);
         String w = edWeight.getText().toString();
         String h = edHeight.getText().toString();
         float weight = Float.parseFloat(w);
         float height = Float.parseFloat(h);
         float bmi = weight / (height * height);
+
         Log.d("MainActivity", "BMI:" + bmi);
+        Toast.makeText(this, "Your BMI : " + bmi, Toast.LENGTH_LONG).show();
+        result.setText("Your BMI : " + bmi);
     }
 }
